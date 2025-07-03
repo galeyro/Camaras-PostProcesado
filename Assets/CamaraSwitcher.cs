f23197a072cf8c29e792195f2ac679884f0bb882
Laboratorio <laboratorio@udla-ec.int>
2025-07-02 21:37:06 -0500

    fix posprocesing

--- Assets/CamaraSwitcher.cs
+++ Assets/CamaraSwitcher.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class CamaraSwitcher : MonoBehaviour
 {
     public Camera[] cameras;
+    public AudioClip[] switchSounds; // 
+    public AudioSource audioSource;  // 
+
     private int currentCameraIndex = 0;
 
     void Start()
     {
-        // Asegura que solo una cámara esté activa al inicio
         SwitchToCamera(currentCameraIndex);
     }
 
     public void SwitchCamera()
     {
-        // Desactiva la cámara actual
+        // Desactiva cámara actual
         cameras[currentCameraIndex].gameObject.SetActive(false);
 
-        // Pasa a la siguiente cámara, vuelve a 0 si es la última
+        // Avanza índice
         currentCameraIndex = (currentCameraIndex + 1) % cameras.Length;
 
-        // Activa la nueva cámara
+        // Activa nueva cámara
         cameras[currentCameraIndex].gameObject.SetActive(true);
+
+        // Reproduce el sonido correspondiente a la cámara actual
+        if (audioSource != null && switchSounds.Length > currentCameraIndex && switchSounds[currentCameraIndex] != null)
+        {
+            audioSource.clip = switchSounds[currentCameraIndex];
+            audioSource.Play();
+        }
     }
 
     private void SwitchToCamera(int index)
